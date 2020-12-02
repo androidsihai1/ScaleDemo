f5dd2e2f65cb1c0cee85e219bcb889c843fe68a3
pengsihai <pengsihai@yy.com>
2020-12-02 18:30:15 +0800

    优化，干掉不要的代码

--- app/src/main/java/com/yy/scaledemo/ScaleImageView.java
+++ app/src/main/java/com/yy/scaledemo/ScaleImageView.java
@@ -11,21 +11,16 @@ import androidx.appcompat.widget.AppCompatImageView;
 
 
 public class ScaleImageView extends AppCompatImageView implements View.OnTouchListener {
-    public static final String TAG = "ScaleX5Webview";
+    public static final String TAG = "ScaleImageView";
     private boolean isCanTouch = false;
     public static final float SCALE_MAX = 5.0f; //最大的缩放比例
     private static final float SCALE_MIN = 1.0f;
-
     private double oldDist = 0;
     private double moveDist = 0;
-
     private float downX1 = 0;
     private float downX2 = 0;
     private float downY1 = 0;
     private float downY2 = 0;
-
-    private float centerX = 0;
-    private float centerY = 0;
 
     public ScaleImageView(Context context) {
         this(context, null);
@@ -44,14 +39,9 @@ public class ScaleImageView extends AppCompatImageView implements View.OnTouchLi
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         Log.d(TAG, "onTouch isCanTouch= " + isCanTouch);
-        if (!isCanTouch) {
-            return false;
-        }
         int pointerCount = event.getPointerCount();
         switch (event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN:
-//                downX1 = event.getX();
-//                downY1 = event.getY();
                 break;
             case MotionEvent.ACTION_UP:
                 if (pointerCount == 2) {
@@ -73,20 +63,12 @@ public class ScaleImageView extends AppCompatImageView implements View.OnTouchLi
                     double changeY1 = y1 - downY1;
                     double changeY2 = y2 - downY2;
 
-
-                    moveDist = spacing(event);
-
-//                    boolean changeMoveDist = Math.abs(moveDist - oldDist) > 15;
-//                    boolean isSameDirectionX = (changeX1 >= 0 && changeX2 >= 0) || (changeX1 <= 0 && changeX2 <= 0);
-//                    boolean isSameDirectionY = (changeY1 >= 0 && changeY2 >= 0) || (changeY1 <= 0 && changeY2 <= 0);
-                    //if (getScaleX() > 1 && (isSameDirectionX && isSameDirectionY) && !changeMoveDist) { //滑动
                     if (getScaleX() > 1) { //滑动
                         float lessX = (float) ((changeX1) / 2 + (changeX2) / 2);
                         float lessY = (float) ((changeY1) / 2 + (changeY2) / 2);
-                        setSelfPivot(lessX, lessY);
+                        setSelfPivot(-lessX, -lessY);
                         Log.d(TAG, "此时为滑动");
                     }
-                    //if (changeMoveDist) { //缩放
                     moveDist = spacing(event);
                     double space = moveDist - oldDist;
                     float scale = (float) (getScaleX() + space / v.getWidth());
@@ -97,40 +79,6 @@ public class ScaleImageView extends AppCompatImageView implements View.OnTouchLi
                     } else {
                         setScale(scale);
                     }
-                    //}
-
-
-//                    if (getScaleX() > 1 && isSameDirectionX && isSameDirectionY) { //滑动
-//                        float lessX = (float) ((downX1 - x1) / 2 + (downX2 - x2) / 2);
-//                        float lessY = (float) ((downY1 - y1) / 2 + (downY2 - y2) / 2);
-//                        setSelfPivot(lessX, lessY);
-//                        Log.d(TAG, "此时为滑动");
-//                    } else if (!isSameDirectionX || !isSameDirectionY) { //缩放
-//                        moveDist = spacing(event);
-//                        double space = moveDist - oldDist;
-//                        double sqrt = Math.sqrt(getWidth() * getWidth() + getHeight() * getHeight());
-//                        float scale = (float) (getScaleX() + space / sqrt);
-//                        if (scale > SCALE_MIN && scale < SCALE_MAX) {
-//                            setScale(scale);
-//                        } else if (scale < SCALE_MIN) {
-//                            setScale(SCALE_MIN);
-//                        }
-//                        Log.d(TAG, "此时为缩放");
-//                    }
-//                    if ((Math.abs(absX1 - absX2) < 20 && Math.abs(absy1 - absy2) < 20) && getScaleX() > 1) { // 滑动
-//                        float lessX = (float) (downX1 - x1);
-//                        float lessY = (float) (downY1 - y2);
-//                        setSelfPivot(lessX, lessY);
-//                    } else {
-//                        moveDist = spacing(event);
-//                        double space = moveDist - oldDist;
-//                        float scale = (float) (getScaleX() + space / getWidth());
-//                        if (scale > SCALE_MIN && scale < SCALE_MAX) {
-//                            setScale(scale);
-//                        } else if (scale < SCALE_MIN) {
-//                            setScale(SCALE_MIN);
-//                        }
-//                    }
                 }
                 break;
             case MotionEvent.ACTION_POINTER_DOWN:
@@ -139,8 +87,6 @@ public class ScaleImageView extends AppCompatImageView implements View.OnTouchLi
                     downX2 = event.getX(1);
                     downY1 = event.getY(0);
                     downY2 = event.getY(1);
-                    centerX = (downX1 + downX2) / 2;
-                    centerY = (downY1 + downY2) / 2;
                     Log.d(TAG, "ACTION_POINTER_DOWN 双指按下 downX1=" + downX1 + " downX2="
                             + downX2 + "  downY1=" + downY1 + " downY2=" + downY2);
                     oldDist = spacing(event); //两点按下时的距离
